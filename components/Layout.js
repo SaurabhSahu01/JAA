c37365b2aaa80ef1bff6cc8865f17d6d4e044515
SaurabhSahu01 <saurabhsahu794@gmail.com>
2023-07-09 00:07:58 +0530

    Basic layout completed

---
+++ components/Layout.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import Head from 'next/head'
+import Header from './Header'
+import Footer from './Footer'
+
+function Layout(props) {
+    return (
+        <>
+            <Head>
+                <title>{props.title ? "JAA - " + props.title : "JAA"}</title>
+                <meta name='description' content='JAA - JNU Alumni Association' />
+                <link rel='icon' href="/favicon.ico" />
+            </Head>
+            <div className='bg-slate-300'>
+                <Header/>
+                <main className="w-full min-h-screen select-none">{props.children}</main>
+                <Footer/>
+            </div>
+        </>
+    )
+}
+
+export default Layout

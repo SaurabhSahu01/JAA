a96f62104ae8dc11e646f07320b5524ead3ab832
Shubham <99029472+Shubh404-SE@users.noreply.github.com>
2023-07-09 23:47:39 +0530

    about page done :X

---
+++ components/About/img.js
@@ -0,0 +1,21 @@
+export const exclusiveimg = [
+    {imgSrc:"/about/t1.jpg", name: "Shri Rakesh Kumar", level:"President", period: "class of 1900-2050"},
+    {imgSrc:"/about/t1.jpg", name: "Shri Rakesh Kumar", level:"President", period: "class of 1900-2050"},
+    {imgSrc:"/about/t1.jpg", name: "Shri Rakesh Kumar", level:"President", period: "class of 1900-2050"},
+    {imgSrc:"/about/t1.jpg", name: "Shri Rakesh Kumar", level:"President", period: "class of 1900-2050"},
+    {imgSrc:"/about/t1.jpg", name: "Shri Rakesh Kumar", level:"President", period: "class of 1900-2050"},
+    {imgSrc:"/about/t1.jpg", name: "Shri Rakesh Kumar", level:"President", period: "class of 1900-2050"},
+    {imgSrc:"/about/t1.jpg", name: "Shri Rakesh Kumar", level:"President", period: "class of 1900-2050"},
+    {imgSrc:"/about/t1.jpg", name: "Shri Rakesh Kumar", level:"President", period: "class of 1900-2050"},
+    {imgSrc:"/about/t1.jpg", name: "Shri Rakesh Kumar", level:"President", period: "class of 1900-2050"},
+    {imgSrc:"/about/t1.jpg", name: "Shri Rakesh Kumar", level:"President", period: "class of 1900-2050"},
+] 
+
+export const developmentimg = [
+    {imgSrc:"/about/t1.jpg", name: "Shri Rakesh Kumar", level:"President", period: "class of 1900-2050"},
+    {imgSrc:"/about/t1.jpg", name: "Shri Rakesh Kumar", level:"President", period: "class of 1900-2050"},
+    {imgSrc:"/about/t1.jpg", name: "Shri Rakesh Kumar", level:"President", period: "class of 1900-2050"},
+    {imgSrc:"/about/t1.jpg", name: "Shri Rakesh Kumar", level:"President", period: "class of 1900-2050"},
+    {imgSrc:"/about/t1.jpg", name: "Shri Rakesh Kumar", level:"President", period: "class of 1900-2050"},
+    {imgSrc:"/about/t1.jpg", name: "Shri Rakesh Kumar", level:"President", period: "class of 1900-2050"},
+]

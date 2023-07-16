f2eb9453b20b581baf461568c46baed212f8200c
Shubham <99029472+Shubh404-SE@users.noreply.github.com>
2023-07-16 14:39:41 +0530

    done :)

--- components/Profile/Input.js
+++ components/Profile/Input.js
@@ -7,7 +7,7 @@ const Input = ({data, state, isdesable, onChangeHandler}) => {
             id={data.id}
             value={state[data.id]}
             placeholder={data.placeholder}
-            className={`bg-gray-200  outline-none border-none p-[2px] ml-2 ${isdesable ? " bg-transparent" : "bg-white"}  `}
+            className={`bg-gray-200 w-fit outline-none border-none p-[2px] ml-2 ${isdesable ? " bg-transparent" : "bg-white"}  `}
             autoComplete="off"
             required
             onChange={onChangeHandler} 

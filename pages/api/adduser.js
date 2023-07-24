ae8fc9059eef838d54aefdddd8d1421797d95f5b
SaurabhSahu01 <saurabhsahu794@gmail.com>
2023-07-24 23:37:31 +0530

    == to ===

--- pages/api/adduser.js
+++ pages/api/adduser.js
@@ -2,7 +2,7 @@ import apimiddleware from "./apimiddleware";
 import { addUser } from "@/src/utils/firebaseadmin";
 
 async function handler(req, res) {
-    if (req.method == "POST") {
+    if (req.method === "POST") {
         const uid = req.body.uid;
         const creationTime = req.body.creationTime;
         const signInType = req.body.signInType;

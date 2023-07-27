be61a770e55cce9c6a17ed56394dc1c7dba486d1
SaurabhSahu01 <saurabhsahu794@gmail.com>
2023-07-27 22:00:58 +0530

    page - feed - jnu feeds

--- components/Feed/JNUNews.js
+++ components/Feed/JNUNews.js
@@ -1,37 +1,15 @@
 import React from 'react'
-import { newsAPI } from '@/firebase.config';
 
 function JNUNews() {
-    const [mount, setMount] = React.useState(false);
-    const [fetchdata, setFetchData] = React.useState(localStorage.getItem('JNUNews'));
-
-    React.useEffect(() => {
-        if (!localStorage.getItem('JNUNews')) {
-            console.log("if")
-            // fetch from the API and store the data in the localStorage
-            fetch(`https://newsapi.org/v2/everything?q=JNU&from=2023-06-27&sortBy=publishedAt&apiKey=${newsAPI}`)
-                .then(res => res.json())
-                .then(data => {
-                    localStorage.setItem('JNUNews', JSON.stringify(data.articles));
-                })
-                .catch(err => {
-                    console.log("error fetching the newsAPI, ", err);
-                })
-        }
-        setMount(true);
-        setFetchData(localStorage.getItem('JNUNews'));
-    }, [fetchdata])
     return (
-        mount && <>
-            <div className='w-full sm:h-[4rem] md:h-[7rem] flex-col justify-center items-center'>
+        <>
+            <div className='sm:w-full lg:w-7/12 md:w-8/12 mx-auto flex-col justify-center items-center'>
                 {
                     localStorage.getItem('JNUNews') && JSON.parse(localStorage.getItem('JNUNews')).map((item, index) => {
                         return (
-                            <div key={index} onClick={() => window.location.href = `${item.url}`} className='flex-col justify-start items-center mb-3 bg-primarycolor text-slate-50'>
-                                <div className='w-full flex justify-center items-center'>
-                                    <img src={item.urlToImage} />
-                                </div>
-                                <p className='text-2xl font-medium'>{item.title}</p>
+                            <div key={index} onClick={() => window.location.href = `${item.url}`} className='w-full h-fit flex-col justify-center items-center mb-3 text-primarycolor cursor-pointer bg-blue-50'>
+                                <img src={item.urlToImage} className='w-full aspect-video object-cover mx-auto object-contain'/>
+                                <p className='text-2xl font-medium text-center'>{item.title}</p>
                             </div>
                         )
                     })

3a0f55c0aee351a46087495ff9cfec11e980cdd1
SaurabhSahu01 <saurabhsahu794@gmail.com>
2023-08-03 19:27:32 +0530

    backend - protected route for registration

--- middleware.js
+++ middleware.js
@@ -11,13 +11,16 @@ export default async function middleware(req) {
     const domain = `${protocol}://${host}`;
     console.log(domain)
 
+    if(userToken && refreshToken && profileSet && (url.includes('/registration'))){
+        return NextResponse.redirect(`${domain}/`);
+    }
     if (!userToken && !refreshToken && (url.includes('/feeds') || url.includes('/jobs') || url.includes('/messages') || url.includes('/join'))) {
         return NextResponse.redirect(`${domain}/login`);
     }
     if(userToken && refreshToken && (url.includes('/login') || url.includes('/signup'))){
         return NextResponse.redirect(`${domain}/`);
     }
-    if(userToken && profileSet === false){
+    if(userToken && refreshToken && profileSet === false){
         return NextResponse.redirect(`${domain}/registration`);
     }
     if(!atkn && (url.includes('/adminpanel'))){

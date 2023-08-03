cbc58ccdad9effc7a52fc29678c3e2a9f614eac5
SaurabhSahu01 <saurabhsahu794@gmail.com>
2023-08-03 17:17:17 +0530

    UI - registration progress line

--- components/Registration/progressLine/ProgressLine.js
+++ components/Registration/progressLine/ProgressLine.js
@@ -1,25 +1,55 @@
 import React from 'react'
 
-const ProgressLine = ({firstStep, secondStep, thirdStep}) => {
+const ProgressLine = ({ firstStep, secondStep }) => {
     return (
-        <div className='flex items-center justify-center pt-10'>
-            <div className={`w-[55px] h-[55px] md:w-[65px] md:h-[65px] bg-blue-600  rounded-full flex items-center justify-center p-2`}>
-                {firstStep && <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-[75%] h-[75%] text-white">
-                    <path strokeLinecap="round" strokeLinejoin="round" d="M4.5 12.75l6 6 9-13.5" />
-                </svg>
-                }
+        <div className='flex items-center justify-center pb-10 '>
+            <div className='relative'>
+                <div className={`w-[45px] h-[45px] bg-blue-600 border-2  rounded-full flex items-center justify-center p-2`}>
+                    {
+                        /* <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-[75%] h-[75%] text-white">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M4.5 12.75l6 6 9-13.5" />
+                        </svg> */
+                        <div className={`w-fit font-bold text-lg text-white  select-none`}>
+                            1
+                        </div>
+                    }
+                </div>
+                <div className=' absolute font-normal text-sm md:w-[80px] text-blue-600'>
+                    User Details
+                </div>
             </div>
-            <div className={`w-[15%] h-2  ${firstStep ? "bg-blue-600" : "bg-gray-100"}`}></div>
-            <div className={`w-[50px] h-[50px] md:w-[65px] md:h-[65px]  ${firstStep ? "bg-blue-600" : "bg-gray-100"} rounded-full flex items-center justify-center p-2`}>
-                {secondStep && <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-[75%] h-[75%] text-white">
-                    <path strokeLinecap="round" strokeLinejoin="round" d="M4.5 12.75l6 6 9-13.5" />
-                </svg>}
+            <div className={`w-[20%] h-1 ml-1 mr-1 rounded-lg bg-gray-400`}></div>
+            <div className=' relative'>
+                <div className={`w-[45px] h-[45px] ${firstStep ? "bg-blue-600 border-2":" bg-gray-200"}  rounded-full flex items-center justify-center p-2`}>
+                    {
+                        /* <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-[75%] h-[75%] text-white">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M4.5 12.75l6 6 9-13.5" />
+                        </svg> */
+                        <div className={`w-fit font-bold text-lg ${firstStep? "text-white":" text-blue-600"}  select-none`}>
+                            2
+                        </div>
+                    }
+                </div>
+                <div className={`absolute font-normal text-sm md:w-[90px] ${firstStep? "text-blue-600":""}`}>
+                    School Details
+                </div>
             </div>
-            <div className={`w-[15%] h-2  ${secondStep ? "bg-blue-600" : "bg-gray-100"}`}></div>
-            <div className={`w-[50px] h-[50px] md:w-[65px] md:h-[65px]  ${secondStep ? "bg-blue-600" : "bg-gray-100"} rounded-full flex items-center justify-center p-2`}>
-                {thirdStep && <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-[75%] h-[75%] text-white">
-                    <path strokeLinecap="round" strokeLinejoin="round" d="M4.5 12.75l6 6 9-13.5" />
-                </svg>}
+
+            <div className={`w-[20%] h-1 ml-1 mr-1 rounded-lg  bg-gray-400`}></div>
+
+            <div className=' relative'>
+                <div className={`w-[45px] h-[45px]  ${secondStep ? "bg-blue-600 border-2" : "bg-gray-200"} rounded-full flex items-center justify-center p-2`}>
+                    {
+                        /* <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-[75%] h-[75%] text-white">
+                           <path strokeLinecap="round" strokeLinejoin="round" d="M4.5 12.75l6 6 9-13.5" />
+                       </svg> */
+                        <div className={`w-fit font-bold text-lg ${secondStep? "text-white":" text-blue-600"}  select-none`}>
+                            3
+                        </div>}
+                </div>
+                <div className={`absolute font-normal text-sm md:w-[90px] ${secondStep ? "text-blue-600":""}`}>
+                    Profile Photo
+                </div>
             </div>
         </div>
     )

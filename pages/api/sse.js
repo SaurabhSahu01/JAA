fc65a6530de152a1bd3ef854224eb36e4f26c43a
SaurabhSahu01 <saurabhsahu794@gmail.com>
2023-08-05 16:47:18 +0530

    sse : auth is remaining

--- pages/api/sse.js
+++ pages/api/sse.js
@@ -1,26 +1,24 @@
 import { db } from "@/src/utils/firebase";
+import { doc, onSnapshot } from 'firebase/firestore';
 
 export default function handler(req, res) {
-    const uid = req.query.UID;
-    // Set response headers for SSE
-    res.setHeader('Content-Type', 'text/event-stream');
-    res.setHeader('Cache-Control', 'no-cache');
-    res.setHeader('Connection', 'keep-alive');
-    res.setHeader('Access-Control-Allow-Origin', '*');
-  
-    // Send an initial event to establish the SSE connection
-    res.write(JSON.stringify({data: 'Connected'}));
-  
-    // Set up a timer to send updates at regular intervals
-    const intervalId = setInterval(() => {
-      const eventData = JSON.stringify({ message: uid });
-      res.write(`data: ${eventData}\n\n`);
-    }, 1000); // Send an update every 5 seconds
-  
-    // Clean up the interval and close the response when the client disconnects
-    req.on('close', () => {
-      clearInterval(intervalId);
-      res.end();
-    });
-  }
-  
+  const uid = req.query.UID;
+  // Set response headers for SSE
+  res.setHeader('Content-Type', 'text/event-stream');
+  res.setHeader('Cache-Control', 'no-cache');
+  res.setHeader('Connection', 'keep-alive');
+  res.setHeader('Access-Control-Allow-Origin', '*');
+
+  // Create the profileRef and set up the real-time listener
+  const profileRef = doc(db, 'users', uid, 'profile', 'profile');
+  const unsubscribe = onSnapshot(profileRef, snapshot => {
+    const eventData = JSON.stringify({ message: snapshot.data() });
+    res.write(`data: ${eventData}\n\n`);
+  });
+
+  // Clean up the listener when the client disconnects
+  req.on('close', () => {
+    unsubscribe();
+    res.end();
+  });
+}

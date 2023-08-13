b45ad7a8c4a33331a5dbe1781c0899c5e3beecbd
SaurabhSahu01 <saurabhsahu794@gmail.com>
2023-08-13 15:44:04 +0530

    UI - upload UI

---
+++ components/Feed/FeedUpload.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function FeedUpload() {
+    const [img, setimg] = React.useState(null);
+    React.useEffect(() => {
+        if(localStorage.getItem('profile')){
+            setimg(JSON.parse(localStorage.getItem('profile'))['photo']);
+        }
+    }, [])
+    return (
+        <div className='w-full flex justify-center items-center gap-3 mt-2'>
+            <img src={`${img}`} alt="profile pic" className='h-[4rem] w-[4rem] rounded-full object-cover xs:hidden md:block'/>
+            <div className='md:w-full xs:w-9/12 sm:p-2 xs:p-1 rounded-full bg-gray-200 flex justify-center items-center border-[1px] border-black cursor-pointer tracking-wider'>
+                Share a post
+            </div>
+        </div>
+    )
+}
+
+export default FeedUpload
--- components/Feed/index.js
+++ components/Feed/index.js
@@ -1,10 +1,10 @@
 import React from 'react'
-
+import FeedUpload from './FeedUpload'
 function Feed() {
   return (
-    <>
-      this is feed section
-    </>
+    <div className='md:w-8/12 lg:w-6/12 xs:w-full mx-auto flex-col justify-center items-center'> 
+      <FeedUpload/>
+    </div>
   )
 }
 

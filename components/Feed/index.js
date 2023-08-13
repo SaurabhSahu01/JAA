8109d93e817a95221ed84da903dfb2532190d9f4
SaurabhSahu01 <saurabhsahu794@gmail.com>
2023-08-13 15:23:38 +0530

    external news removed

--- components/Feed/index.js
+++ components/Feed/index.js
@@ -1,31 +1,9 @@
 import React from 'react'
-import JNUNews from './JNUNews'
-import { newsAPI } from '@/firebase.config';
 
 function Feed() {
-  const [mount, setMount] = React.useState(false);
-  React.useEffect(() => {
-    if (!localStorage.getItem('JNUNews')) {
-      console.log("if")
-      // fetch from the API and store the data in the localStorage
-      fetch(`https://newsapi.org/v2/everything?q=JNU&from=2023-06-27&sortBy=publishedAt&apiKey=${newsAPI}`)
-        .then(res => res.json())
-        .then(data => {
-          console.log(data);
-          localStorage.setItem('JNUNews', JSON.stringify(data.articles));
-          setMount(true);
-        })
-        .catch(err => {
-          console.log("error fetching the newsAPI, ", err);
-        })
-    }
-    else{
-      setMount(true);
-    }
-  }, [])
   return (
-    mount && <>
-      <JNUNews />
+    <>
+      this is feed section
     </>
   )
 }

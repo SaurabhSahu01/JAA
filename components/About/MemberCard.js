c942962728c82f7524bbea06fbb5807d6e945530
Shubham <99029472+Shubh404-SE@users.noreply.github.com>
2023-08-15 16:05:26 +0530

    about page redesigned

--- components/About/MemberCard.js
+++ components/About/MemberCard.js
@@ -3,10 +3,10 @@ import React from 'react'
 const MemberCard = ({data}) => {
     const {imgSrc, name, level, period } = data;
   return (
-    <div className=' flex flex-col justify-center items-center gap-2 bg-white rounded-md p-2 m-2 w-fit shadow-md'>
-        <img src={imgSrc} alt="" className='rounded-full w-[150px] h-[150px]' />
-        <div className=' py-1 font-medium text-sm text-left px-2'>
-            <p>{name}</p>
+    <div className=' flex flex-col justify-center items-center gap-2 bg-[#F5F5F5] rounded-md p-4 m-2 w-fit shadow-md'>
+        <img src={imgSrc} alt="" className='rounded-full w-[100px] sm:w-[150px] h-[100px] sm:h-[150px] object-cover' />
+        <div className=' py-1 font-medium text-base text-left px-2'>
+            <p className=' underline'>{name}</p>
             <p>{level}</p>
             <p>{period}</p>
         </div>

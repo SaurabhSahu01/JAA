2c4616b64159519401d83263b0bd50e61ad1e6d0
Shubham <99029472+Shubh404-SE@users.noreply.github.com>
2023-08-19 15:32:30 +0530

    contact dummy field filled

--- components/Contact/index.js
+++ components/Contact/index.js
@@ -34,7 +34,7 @@ function Contact() {
           <div className='w-full md:w-6/12 bg-[#1B2D56] rounded-md'>
             <div className='text-center px-6 py-2'>
               <h3 className='text-2xl font-bold'>Contact Informations</h3>
-              {/* <p className='text-sm '>say somthing to start a live chat</p> */}
+              <p className='text-sm '>say somthing to start a live chat</p>
             </div>
 
             <div className='flex flex-col content-start m-10 '>
@@ -42,24 +42,14 @@ function Contact() {
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                   <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 002.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 01-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 00-1.091-.852H4.5A2.25 2.25 0 002.25 4.5v2.25z" />
                 </svg>
-                <a className='mx-3 font-light text-sm' href="tel:+91564564"><span>+91-85494891</span></a>
-                <a className='mx-3 font-light text-sm' href="tel:+918946542"><span>+91-856146548</span></a>
+                <a className='mx-3 font-light text-sm' href="tel:+919717129297"><span>+91-9717129297</span></a>
               </div>
 
               <div className='flex my-4'>
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                   <path strokeLinecap="round" strokeLinejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25m19.5 0v.243a2.25 2.25 0 01-1.07 1.916l-7.5 4.615a2.25 2.25 0 01-2.36 0L3.32 8.91a2.25 2.25 0 01-1.07-1.916V6.75" />
                 </svg>
-                <a className='mx-3 font-light text-sm' href="mailto: demo@gmail.com"><span>md.abcdf@gmail.com</span></a>
-              </div>
-
-              <div className='w-full flex my-4'>
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                  <path strokeLinecap="round" strokeLinejoin="round" d="M7.875 14.25l1.214 1.942a2.25 2.25 0 001.908 1.058h2.006c.776 0 1.497-.4 1.908-1.058l1.214-1.942M2.41 9h4.636a2.25 2.25 0 011.872 1.002l.164.246a2.25 2.25 0 001.872 1.002h2.092a2.25 2.25 0 001.872-1.002l.164-.246A2.25 2.25 0 0116.954 9h4.636M2.41 9a2.25 2.25 0 00-.16.832V12a2.25 2.25 0 002.25 2.25h15A2.25 2.25 0 0021.75 12V9.832c0-.287-.055-.57-.16-.832M2.41 9a2.25 2.25 0 01.382-.632l3.285-3.832a2.25 2.25 0 011.708-.786h8.43c.657 0 1.281.287 1.709.786l3.284 3.832c.163.19.291.404.382.632M4.5 20.25h15A2.25 2.25 0 0021.75 18v-2.625c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125V18a2.25 2.25 0 002.25 2.25z" />
-                </svg>
-                <span className='w-fit mx-3 font-light text-sm'>us bro us,
-                  jnu bro jnu,
-                </span>
+                <a className='mx-3 font-light text-sm' href="mailto: connect@jnualumniassociation.com"><span>connect@jnualumniassociation.com</span></a>
               </div>
             </div>
 

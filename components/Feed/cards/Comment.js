11c80dbe74202678f605f21531af1a8ef3c06a7a
SaurabhSahu01 <saurabhsahu794@gmail.com>
2023-08-20 13:25:29 +0530

    comments feature in posts

--- components/Feed/cards/Comment.js
+++ components/Feed/cards/Comment.js
@@ -27,7 +27,7 @@ const Comment = ({ data }) => {
                 {user?.photo ? <img src={user?.photo} alt="user" className='w-7 h-7 object-cover mr-2 rounded-full cursor-pointer' /> :
                     <img src='/icons/profileIcon.png' className='w-8 h-8 rounded-full mr-2' alt='profilepic'/>}
                 <div className='relative w-full'>
-                    <p className=' font-normal text-sm'><span className=' font-medium cursor-pointer'>{user?.name}</span> {comment}</p>
+                    <p className=' font-normal text-sm'><span className=' font-medium cursor-pointer hover:text-blue-500 text-slate-500'>{user?.name}</span> <span className='text-primarycolor font-medium'>{comment}</span></p>
                     <p className=' font-light text-xs text-gray-500'>{date[0]+date[1]}</p>
                 </div>
             </div>

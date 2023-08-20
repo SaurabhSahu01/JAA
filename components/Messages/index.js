70c80c97d3784574fe6a7ee6b80d129d2307960e
SaurabhSahu01 <saurabhsahu794@gmail.com>
2023-08-20 16:10:15 +0530

    frontend - protected route in join

--- components/Messages/index.js
+++ components/Messages/index.js
@@ -1,8 +1,37 @@
 import React from 'react'
+import { db } from '@/src/utils/firebase';
+import { onSnapshot, doc, collection, getDoc } from "firebase/firestore"
+import cookieCutter from 'cookie-cutter';
+import { useRouter } from 'next/router';
+import { toast, Toaster } from "react-hot-toast";
 
 function Messages() {
+  const router = useRouter();
+  React.useEffect(() => {
+    const uid = cookieCutter.get('uid');
+    const documentRef = doc(db, 'users', uid);
+    getDoc(documentRef).then(docSnapshot => {
+      if (docSnapshot.exists()) {
+        const data = docSnapshot.data();
+        if (data.verified === false || data.verified === 'pending') {
+          toast.error('Please Join the Alumni Family!');
+          setTimeout(() => {
+            router.push('/join');
+          }, 1000)
+        }
+      }
+      else {
+        console.log("document does not exists");
+      }
+    }).catch(err => {
+      console.log('error getting the user verification details');
+    })
+  }, [])
   return (
-    <div className='text-2xl font-bold'>Coming Soon...</div>
+    <>
+      <div className='text-2xl font-bold'>Coming Soon...</div>
+      <Toaster toastOptions={{ duration: 2000 }} position="top-center" />
+    </>
   )
 }
 

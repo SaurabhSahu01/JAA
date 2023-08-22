d93444f337f40961773391734d7d4221f923f511
SaurabhSahu01 <saurabhsahu794@gmail.com>
2023-08-22 16:26:12 +0530

    join completed

--- components/Join/index.js
+++ components/Join/index.js
@@ -3,17 +3,21 @@ import Typewriter from 'typewriter-effect'
 import cookieCutter from "cookie-cutter";
 import { toast, Toaster } from 'react-hot-toast';
 import { db } from '@/src/utils/firebase';
-import { doc, collection, getDoc } from "firebase/firestore"
+import { doc, getDoc } from "firebase/firestore"
+import Loader from '../common/Loader';
+import { useRouter } from 'next/router';
 
 function Join() {
-
+  const router = useRouter();
   const [reciept, setReciept] = React.useState(null);
   const [proofJNU, setProofJNU] = React.useState(null);
-  const [uploaded, setUploaded] = React.useState(false);
   const [status, setStatus] = React.useState(false);
+  const [loading, setLoading] = React.useState(false);
+  const [showToast, setShowToast] = React.useState(true);
 
   const getVerified = async () => {
     if (reciept && proofJNU) {
+      setLoading(true);
       // send it to admin with user id...
       const formData = new FormData();
       formData.append('image1', reciept);
@@ -29,21 +33,25 @@ function Join() {
         console.log(res);
         setProofJNU(null)
         setReciept(null);
+        setLoading(false);
       }).catch((err) => {
         console.log(err);
       });
     }
   }
   React.useEffect(() => {
+    setTimeout(() => {
+      setShowToast(false);
+    }, 2000)
     const uid = cookieCutter.get('uid');
     const documentRef = doc(db, 'users', uid);
     getDoc(documentRef).then(docSnapshot => {
-      if(docSnapshot.exists()){
+      if (docSnapshot.exists()) {
         const data = docSnapshot.data();
-        if(data.verified === true){
+        if (data.verified === true) {
           setStatus(true);
         }
-        else if(data.verified === 'pending'){
+        else if (data.verified === 'pending') {
           setStatus('pending')
         }
       }
@@ -91,9 +99,12 @@ function Join() {
   return (
     (status === false) ? <div className='h-screen w-full'>
       <div>
-        <Toaster toastOptions={{ duration: 1000 }} position='top-center' />
+        {showToast && <Toaster toastOptions={{ duration: 1000 }} position='top-center' />}
+        {loading && <div className='h-screen w-full fixed top-0 right-0 z-[200] backdrop-blur-sm'>
+          <Loader color="#1B2D56" loading={loading} size={70} />
+        </div>}
         <div className='flex flex-col items-center'>
-          <p className='text-3xl font-semibold text-primarycolor tracking-wider'>JNU Alumni Association</p>
+          <p className='md:text-3xl xs:text-2xl font-semibold text-primarycolor tracking-wider text-center'>JNU Alumni Association</p>
           <div className='text-sm text-primarycolor font-light'>
             <Typewriter
               options={{
@@ -107,8 +118,8 @@ function Join() {
 
 
 
-        <div className=' w-11/12 sm:w-5/6 mx-auto mt-10 bg-white rounded-md p-4'>
-          <h1 className=' font-semibold text-2xl text-blue-400'>Rs.500/- to join website membership as per below Bank Details and upload reciept & a proof of a jnu student</h1>
+        <div className=' w-11/12 sm:w-5/6 mx-auto mt-10 bg-slate-200/50 backdrop-blur-sm rounded-md p-4'>
+          <h1 className=' font-semibold md:text-xl xs:text-xl text-red-400'>Rs.500/- to join website membership as per below Bank Details and upload reciept & a proof of a jnu student</h1>
           <div className=' mt-10'>
             <h1 className='text-2xl font-semibold '>Bank Details:</h1>
             <div className=' font-light text-base leading-6'>
@@ -131,7 +142,7 @@ function Join() {
                   setReciept(e.target.files[0]);
                 }}
               />
-              <label htmlFor="document" className=' p-2 bg-blue-400 hover:bg-blue-300 rounded-md text-base font-medium cursor-pointer inline-block w-max'>Upload Reciept here</label>
+              <label htmlFor="document" className=' p-2 bg-primarycolor text-white rounded-md text-base font-medium cursor-pointer inline-block w-max'>Upload Payment Receipt</label>
               {reciept && <p className=' text-xs font-light text-center absolute -bottom-4 right-[50%] translate-x-[50%]'>1 file uploded</p>}
             </div>
             <div className='relative w-min'>
@@ -144,28 +155,31 @@ function Join() {
                   setProofJNU(e.target.files[0]);
                 }}
               />
-              <label htmlFor="proof" className=' p-2 bg-blue-400 hover:bg-blue-300 rounded-md text-base font-medium cursor-pointer inline-block w-max'>Upload Proof here</label>
+              <label htmlFor="proof" className=' p-2 bg-primarycolor text-white rounded-md text-base font-medium cursor-pointer inline-block w-max'>Upload JNU Alumni Proof</label>
               {proofJNU && <p className=' text-xs font-light text-center absolute -bottom-4 right-[50%] translate-x-[50%] w-max '>1 file uploded</p>}
             </div>
             <button
-              className={` ${!reciept ? " cursor-not-allowed" : " cursor-pointer"} py-2 px-3 bg-slate-400 hover:bg-slate-300 text-base rounded-lg`}
-              onClick={getVerified}
+              className={` ${(!reciept || !proofJNU) ? " cursor-not-allowed text-black bg-slate-300" : " cursor-pointer bg-primarycolor text-white"} py-2 px-3 text-base rounded-lg`}
+              onClick={async () => {
+                await getVerified();
+                router.push('/join');
+              }}
             >Submit</button>
           </div>
         </div>
 
 
       </div>
-    </div> : (status === true) ? 
-    // for status === true
-    <div className='h-screen w-full flex flex-row items-center justify-center'>
-      <p className='md:text-3xl xs:text-xl p-2 animate-bounce font-semibold text-center text-green-600'>Congratulations! You are now a member of the JNU Alumni Association Family 🎉</p>
-    </div> : 
-    (status === 'pending') && 
-    // for status === 'pending'
-    <div className='h-screen w-full flex flex-row items-center justify-center'>
-    <p className='md:text-3xl xs:text-xl p-2 animate-bounce font-semibold text-center text-primarycolor'>Your documents have been sent for the verification! Please wait till the verification is done. 🙏🏻</p>
-  </div>
+    </div> : (status === true) ?
+      // for status === true
+      <div className='h-screen w-full flex flex-row items-center justify-center'>
+        <p className='md:text-3xl xs:text-xl p-2 animate-bounce font-semibold text-center text-green-600'>Congratulations! You are now a member of the JNU Alumni Association Family 🎉</p>
+      </div> :
+      (status === 'pending') &&
+      // for status === 'pending'
+      <div className='h-screen w-full flex flex-row items-center justify-center'>
+        <p className='md:text-3xl xs:text-xl p-2 animate-bounce font-semibold text-center text-primarycolor'>Your documents have been sent for the verification! Please wait till the verification is done. 🙏🏻</p>
+      </div>
   )
 }
 

5de3446bc55da576291a573d3f4239fec1f6b56e
SaurabhSahu01 <saurabhsahu794@gmail.com>
2023-08-22 15:15:07 +0530

    admin -> user verification implemented

--- components/Admin/Users/Verify.js
+++ components/Admin/Users/Verify.js
@@ -22,20 +22,43 @@ function Verify({ userid }) {
                 <img src={data.image1} alt="receipt" className='w-full h-fit'></img>
                 <img src={data.image2} alt="receipt" className='w-full h-fit'></img>
             </div>
-            <button className='px-5 py-3 rounded-md bg-green-500 text-white' onClick={() => {
-                fetch(`/api/verifyuser?uid=${userid}`, {
-                    method: "GET",
-                    headers: {
-                        'Content-type': 'application/json; charset=UTF-8',
-                        "authorization": `Bearer ${cookieCutter.get('atkn')}`
-                    }
-                }).then(res => res.json()).then(data => {
-                    console.log(data);
-                    setTimeout(() => {
-                        router.push('/adminpanel/users');
-                    }, 500)
-                }).catch(err => console.error(err));
-            }}>Verify</button>
+            <div className='w-full flex flex-row justify-center items-center gap-5'>
+                <button className='px-5 py-3 rounded-md bg-green-500 text-white' onClick={() => {
+                    fetch(`/api/verifyuser?uid=${userid}`, {
+                        method: "POST",
+                        headers: {
+                            'Content-type': 'application/json; charset=UTF-8',
+                            "authorization": `Bearer ${cookieCutter.get('atkn')}`
+                        },
+                        body: JSON.stringify({
+                            action: "verify"
+                        })
+                    }).then(res => res.json()).then(data => {
+                        console.log(data);
+                        setTimeout(() => {
+                            router.push('/adminpanel/users');
+                        }, 500)
+                    }).catch(err => console.error(err));
+                }}>Verify</button>
+                <button className='px-5 py-3 rounded-md bg-red-500 text-white' onClick={() => {
+                    fetch(`/api/verifyuser?uid=${userid}`, {
+                        method: "POST",
+                        headers: {
+                            'Content-type': 'application/json; charset=UTF-8',
+                            "authorization": `Bearer ${cookieCutter.get('atkn')}`
+                        },
+                        body: JSON.stringify({
+                            action: "not verify"
+                        })
+                    }).then(res => res.json()).then(data => {
+                        console.log(data);
+                        setTimeout(() => {
+                            router.push('/adminpanel/users');
+                        }, 500)
+                    }).catch(err => console.error(err));
+                }}>Do Not Verify</button>
+
+            </div>
         </div>
     )
 }
--- pages/api/verifyuser.js
+++ pages/api/verifyuser.js
@@ -2,16 +2,28 @@ import adminmiddleware from "./adminmiddleware";
 import { db } from "@/src/utils/firebaseadmin";
 
 async function handler(req, res){
-    if(req.method === "GET"){
+    if(req.method === "POST"){
         const uid = req.query.uid;
+        const { action } = req.body;
         try{
-            db.collection('users').doc(uid).set({
-                verified: true
-            }, {merge: true}).then(response => {
-                res.status(200).json({
-                    message: `${uid} user verified`
+            if(action === "verify") {
+                db.collection('users').doc(uid).set({
+                    verified: true
+                }, {merge: true}).then(response => {
+                    res.status(200).json({
+                        message: `${uid} user verified`
+                    })
                 })
-            })
+            }
+            else if(action === "not verify"){
+                db.collection('users').doc(uid).set({
+                    verified: false
+                }, {merge: true}).then(response => {
+                    res.status(200).json({
+                        message: `${uid} user is not verified`
+                    })
+                })
+            }
         }
         catch(err){
             res.status(504).json({
@@ -22,7 +34,7 @@ async function handler(req, res){
     else{
         res.status(405).json({
             status: 405,
-            message: "only GET allowed"
+            message: "only POST allowed"
         })
     }
 }

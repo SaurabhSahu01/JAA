b90616da44658ff8e965de1ceebe070ac1517686
SaurabhSahu01 <saurabhsahu794@gmail.com>
2023-08-23 14:46:49 +0530

    chatAt(0) error in Sidebar chat Chat.js resolved

--- components/Messages/Sidebar/chat/Chat.js
+++ components/Messages/Sidebar/chat/Chat.js
@@ -20,7 +20,7 @@ const Chat = ({ data, selectChatUser }) => {
         }
         getProfile();
     }, []);
-
+    const fullName = profile?.firstName.charAt(0).toUpperCase() + profile?.firstName.slice(1) + " " + profile?.lastName.charAt(0).toUpperCase() + profile?.lastName.slice(1);
     return (
         <div
             onClick={() => selectChatUser(profile)}
@@ -29,7 +29,7 @@ const Chat = ({ data, selectChatUser }) => {
             {profile?.photo ? <img src={profile?.photo} className="w-[50px] h-[50px] rounded-full object-cover" /> : <img src="/icons/profileIcon.png" className="w-[50px] h-[50px]" />}
             {profile && <div className=" flex flex-col justify-center">
                 <span className=" text-base text-black flex items-center justify-between">
-                    <div className=" font-medium text-lg">{profile?.firstName.charAt(0).toUpperCase() + profile?.firstName.slice(1)} {profile?.lastName.charAt(0).toUpperCase() + profile?.lastName.slice(1)}</div>
+                    <div className=" font-medium text-lg">{fullName}</div>
                 </span>
                 <p className=" text-sm text-gray-500">{profile?.school}</p>
             </div>}

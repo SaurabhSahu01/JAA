e34ec4f124ec6b251bb9c63a2f4ad4e55313beee
SaurabhSahu01 <saurabhsahu794@gmail.com>
2023-08-24 16:30:10 +0530

    home page major changes done

---
+++ pages/api/getuserevents.js
@@ -0,0 +1,23 @@
+import apimiddleware from "./apimiddleware";
+import { db } from "@/src/utils/firebaseadmin";
+async function handler(req, res) {
+    if (req.method === "GET") {
+        const eventRef = db.collection('events').get();
+        const eventDocs = (await eventRef).docs;
+        const data = [];
+        for(const doc of eventDocs){
+            data.push({id: doc.id, data: doc.data()});
+        }
+        res.status(200).json({
+            data: data
+        })
+    }
+    else {
+        res.status(405).json({
+            status: 405,
+            message: "only GET allowed"
+        })
+    }
+}
+
+export default apimiddleware(handler)

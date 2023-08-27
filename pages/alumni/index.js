3637d849ad5d70d76764b4c85df8694f6ca5ca0d
SaurabhSahu01 <saurabhsahu794@gmail.com>
2023-08-27 13:20:06 +0530

    alumni page

---
+++ pages/alumni/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Layout from '@/components/Layout'
+import Alumni from '@/components/Alumni'
+
+function AlumniPage() {
+    return (
+        <Layout title="alumni">
+            <Alumni />
+        </Layout>
+    )
+}
+
+export default AlumniPage

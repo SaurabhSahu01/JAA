a1f0a07d55a9beb39d071fcc3073af479b21e655
Shubham <99029472+Shubh404-SE@users.noreply.github.com>
2023-08-28 10:02:13 +0530

    about me dummy user laga diya hu

--- components/About/img.js
+++ components/About/img.js
@@ -1,18 +1,18 @@
 export const exclusiveimg = [
-    {imgSrc:"/about/t1.webp", name: "Shri Rakesh Kumar", level:"President", period: "class of 1900-2050"},
-    {imgSrc:"/about/t1.webp", name: "Shri Rakesh Kumar", level:"President", period: "class of 1900-2050"},
-    {imgSrc:"/about/t1.webp", name: "Shri Rakesh Kumar", level:"President", period: "class of 1900-2050"},
-    {imgSrc:"/about/t1.webp", name: "Shri Rakesh Kumar", level:"President", period: "class of 1900-2050"},
-    {imgSrc:"/about/t1.webp", name: "Shri Rakesh Kumar", level:"President", period: "class of 1900-2050"},
-    {imgSrc:"/about/t1.webp", name: "Shri Rakesh Kumar", level:"President", period: "class of 1900-2050"},
-    {imgSrc:"/about/t1.webp", name: "Shri Rakesh Kumar", level:"President", period: "class of 1900-2050"},
-    {imgSrc:"/about/t1.webp", name: "Shri Rakesh Kumar", level:"President", period: "class of 1900-2050"},
+    {imgSrc:"/icons/profileIcon.webp", name: "Shri Rakesh Kumar", level:"President", period: "class of 1900-2050"},
+    {imgSrc:"/icons/profileIcon.webp", name: "Shri Rakesh Kumar", level:"President", period: "class of 1900-2050"},
+    {imgSrc:"/icons/profileIcon.webp", name: "Shri Rakesh Kumar", level:"President", period: "class of 1900-2050"},
+    {imgSrc:"/icons/profileIcon.webp", name: "Shri Rakesh Kumar", level:"President", period: "class of 1900-2050"},
+    {imgSrc:"/icons/profileIcon.webp", name: "Shri Rakesh Kumar", level:"President", period: "class of 1900-2050"},
+    {imgSrc:"/icons/profileIcon.webp", name: "Shri Rakesh Kumar", level:"President", period: "class of 1900-2050"},
+    {imgSrc:"/icons/profileIcon.webp", name: "Shri Rakesh Kumar", level:"President", period: "class of 1900-2050"},
+    {imgSrc:"/icons/profileIcon.webp", name: "Shri Rakesh Kumar", level:"President", period: "class of 1900-2050"},
 ] 
 
 export const developmentimg = [
-    {imgSrc:"/about/t1.webp", name: "Shri Rakesh Kumar", level:"President", period: "class of 1900-2050"},
-    {imgSrc:"/about/t1.webp", name: "Shri Rakesh Kumar", level:"President", period: "class of 1900-2050"},
-    {imgSrc:"/about/t1.webp", name: "Shri Rakesh Kumar", level:"President", period: "class of 1900-2050"},
-    {imgSrc:"/about/t1.webp", name: "Shri Rakesh Kumar", level:"President", period: "class of 1900-2050"},
-    {imgSrc:"/about/t1.webp", name: "Shri Rakesh Kumar", level:"President", period: "class of 1900-2050"},
+    {imgSrc:"/icons/profileIcon.webp", name: "Shri Rakesh Kumar", level:"President", period: "class of 1900-2050"},
+    {imgSrc:"/icons/profileIcon.webp", name: "Shri Rakesh Kumar", level:"President", period: "class of 1900-2050"},
+    {imgSrc:"/icons/profileIcon.webp", name: "Shri Rakesh Kumar", level:"President", period: "class of 1900-2050"},
+    {imgSrc:"/icons/profileIcon.webp", name: "Shri Rakesh Kumar", level:"President", period: "class of 1900-2050"},
+    {imgSrc:"/icons/profileIcon.webp", name: "Shri Rakesh Kumar", level:"President", period: "class of 1900-2050"},
 ]

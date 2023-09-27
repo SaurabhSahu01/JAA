dd589abcfc02f382f1ccec8e36d45527755d5bdd
Shubh404-SE <tanwarshubham29@gmail.com>
2023-09-27 20:56:18 +0530

    all images listed in gallery

--- components/Gallery/data.js
+++ components/Gallery/data.js
@@ -26,11 +26,8 @@ const images = [
             "/gallery/jnu/1649242971639.webp",
             "/gallery/jnu/1649242971649.webp",
             "/gallery/jnu/1649242971867.webp",
-            // "/gallery/jnu/1649242971879.webp",
-            // "/gallery/jnu/WA0011.webp",
             "/gallery/jnu/WA0012.webp",
             "/gallery/jnu/WA0013.webp",
-            // "/gallery/jnu/WA0000.webp",
             "/gallery/jnu/WA0001.webp",
             "/gallery/jnu/snapchat.webp",
         ]
@@ -38,18 +35,388 @@ const images = [
     {
         "category": "events",
         "images": [
-            "/gallery/events/wp1.webp",
-            // "/gallery/events/IMG_2083.webp",
-            // "/gallery/events/IMG_2084.webp",
-            // "/gallery/events/wa0008.webp",
-            // "/gallery/events/wa0027.webp",
-            // "/gallery/events/wa0016.webp",
+
+            "/gallery/events/eventpic1.webp",
+
+            "/gallery/events/eventpic2.webp",
+
+            "/gallery/events/eventpic3.webp",
+
+            "/gallery/events/eventpic4.webp",
+
+
+            "/gallery/events/eventpic5.webp",
+
+            "/gallery/events/eventpic6.webp",
+
+            "/gallery/events/eventpic7.webp",
+
+            "/gallery/events/eventpic8.webp",
+
+
+            "/gallery/events/eventpic9.webp",
+
+
+            "/gallery/events/eventpic10.webp",
+
+
+            "/gallery/events/eventpic11.webp",
+
+
+            "/gallery/events/eventpic12.webp",
+
+
+            "/gallery/events/eventpic13.webp",
+
+
+            "/gallery/events/eventpic14.webp",
+
+
+            "/gallery/events/eventpic15.webp",
+
+
+            "/gallery/events/eventpic16.webp",
+
+
+
+            "/gallery/events/eventpic17.webp",
+
+
+
+            "/gallery/events/eventpic18.webp",
+
+
+            "/gallery/events/eventpic19.webp",
+
+
+            "/gallery/events/eventpic20.webp",
+
+
+            "/gallery/events/eventpic21.webp",
+
+
+            "/gallery/events/eventpic22.webp",
+
+
+            "/gallery/events/eventpic23.webp",
+
+
+            "/gallery/events/eventpic24.webp",
+
+
+            "/gallery/events/eventpic25.webp",
+
+
+            "/gallery/events/eventpic26.webp",
+
+
+            "/gallery/events/eventpic27.webp",
+
+
+
+            "/gallery/events/eventpic28.webp",
+
+
+            "/gallery/events/eventpic29.webp",
+
+
+            "/gallery/events/eventpic30.webp",
+
+
+            "/gallery/events/eventpic31.webp",
+
+
+            "/gallery/events/eventpic32.webp",
+
+
+            "/gallery/events/eventpic33.webp",
+
+
+            "/gallery/events/eventpic34.webp",
+
+
+
+            "/gallery/events/eventpic35.webp",
+
+
+            "/gallery/events/eventpic36.webp",
+
+
+            "/gallery/events/eventpic37.webp",
+
+
+            "/gallery/events/eventpic38.webp",
+
+
+            "/gallery/events/eventpic39.webp",
+
+
+            "/gallery/events/eventpic40.webp",
+
+
+            "/gallery/events/eventpic41.webp",
+
+
+            "/gallery/events/eventpic42.webp",
+
+
+            "/gallery/events/eventpic43.webp",
+
+
+            "/gallery/events/eventpic44.webp",
+
+
+            "/gallery/events/eventpic45.webp",
+
+
+            "/gallery/events/eventpic46.webp",
+
+
+            "/gallery/events/eventpic47.webp",
+
+
+            "/gallery/events/eventpic48.webp",
+
+
+            "/gallery/events/eventpic49.webp",
+
+
+            "/gallery/events/eventpic50.webp",
+
+
+            "/gallery/events/eventpic51.webp",
+
+
+            "/gallery/events/eventpic52.webp",
+
+
+            "/gallery/events/eventpic53.webp",
+
+
+            "/gallery/events/eventpic54.webp",
+
+
+            "/gallery/events/eventpic55.webp",
+
+
+            "/gallery/events/eventpic56.webp",
+
+
+            "/gallery/events/eventpic57.webp",
+
+
+            "/gallery/events/eventpic58.webp",
+
+
+
+            "/gallery/events/eventpic59.webp",
+
+
+            "/gallery/events/eventpic60.webp",
+
+
+            "/gallery/events/eventpic61.webp",
+
+
+            "/gallery/events/eventpic62.webp",
+
+
+            "/gallery/events/eventpic63.webp",
+
+
+            "/gallery/events/eventpic64.webp",
+
+
+
+            "/gallery/events/eventpic65.webp",
+
+
+            "/gallery/events/eventpic66.webp",
+
+
+            "/gallery/events/eventpic67.webp",
+
+
+            "/gallery/events/eventpic68.webp",
+
+
+            "/gallery/events/eventpic69.webp",
+
+
+            "/gallery/events/eventpic70.webp",
+
+
+            "/gallery/events/eventpic71.webp",
+
+
+            "/gallery/events/eventpic72.webp",
+
+
+            "/gallery/events/eventpic73.webp",
+
+            "/gallery/events/eventpic74.webp",
+
+
+            "/gallery/events/eventpic75.webp",
+
+
+            "/gallery/events/eventpic76.webp",
+
+
+
+            "/gallery/events/eventpic77.webp",
+
+
+            "/gallery/events/eventpic78.webp",
+
+
+
+            "/gallery/events/eventpic79.webp",
+
+
+            "/gallery/events/eventpic80.webp",
+
+
+            "/gallery/events/eventpic81.webp",
+
+
+
+            "/gallery/events/eventpic82.webp",
+
+
+
+            "/gallery/events/eventpic83.webp",
+
+
+
+            "/gallery/events/eventpic84.webp",
+
+
+            "/gallery/events/eventpic85.webp",
+
+
+
+            "/gallery/events/eventpic86.webp",
+
+
+            "/gallery/events/eventpic87.webp",
+
+
+
+            "/gallery/events/eventpic88.webp",
+
+
+
+            "/gallery/events/eventpic89.webp",
+
+
+            "/gallery/events/eventpic90.webp",
+
+
+            "/gallery/events/eventpic91.webp",
+
+
+
+            "/gallery/events/eventpic92.webp",
+
+
+
+            "/gallery/events/eventpic93.webp",
+
+
+
+            "/gallery/events/eventpic94.webp",
+
+
+            "/gallery/events/eventpic95.webp",
+
+
+            "/gallery/events/eventpic96.webp",
+
+            "/gallery/events/eventpic97.webp",
+
+            "/gallery/events/eventpic98.webp",
+
+            "/gallery/events/eventpic99.webp",
+
+            "/gallery/events/eventpic100.webp",
+            "/gallery/events/eventpic101.webp",
+            "/gallery/events/eventpic102.webp",
+            "/gallery/events/eventpic103.webp",
+            "/gallery/events/eventpic104.webp",
+            "/gallery/events/eventpic105.webp",
+            "/gallery/events/eventpic106.webp",
+            "/gallery/events/eventpic107.webp",
+            "/gallery/events/eventpic108.webp",
+            "/gallery/events/eventpic109.webp",
+            "/gallery/events/eventpic110.webp",
+            "/gallery/events/eventpic111.webp",
+            "/gallery/events/eventpic112.webp",
+            "/gallery/events/eventpic113.webp",
+            "/gallery/events/eventpic114.webp",
+            "/gallery/events/eventpic115.webp",
+            "/gallery/events/eventpic116.webp",
+            "/gallery/events/eventpic117.webp",
+            "/gallery/events/eventpic118.webp",
+            "/gallery/events/eventpic119.webp",
+            "/gallery/events/eventpic120.webp",
+            "/gallery/events/eventpic121.webp",
+            "/gallery/events/eventpic122.webp",
+            "/gallery/events/eventpic123.webp",
+            "/gallery/events/eventpic124.webp",
+            "/gallery/events/eventpic125.webp",
+            "/gallery/events/eventpic126.webp",
+            "/gallery/events/eventpic127.webp",
+
+            "/gallery/events/eventpic128.webp",
+
+            "/gallery/events/eventpic129.webp",
+
+            "/gallery/events/eventpic130.webp",
+
+            "/gallery/events/eventpic131.webp",
+
+            "/gallery/events/eventpic132.webp",
+
+            "/gallery/events/eventpic133.webp",
+
+            "/gallery/events/eventpic134.webp",
+
+            "/gallery/events/eventpic135.webp",
+
+            "/gallery/events/eventpic136.webp",
+
+            "/gallery/events/eventpic137.webp",
+
+            "/gallery/events/eventpic138.webp",
+
+            "/gallery/events/eventpic139.webp",
+
+            "/gallery/events/eventpic140.webp",
+
+            "/gallery/events/eventpic141.webp",
+
+            "/gallery/events/eventpic142.webp",
+
+            "/gallery/events/eventpic143.webp",
+
+            "/gallery/events/eventpic144.webp",
+
+            "/gallery/events/eventpic145.webp",
+
+            "/gallery/events/eventpic146.webp",
+
+            "/gallery/events/eventpic147.webp",
+
+            "/gallery/events/eventpic148.webp",
+
         ]
     },
 
     {
         "category": "alumni meets",
         "images": [
+
             // '/gallery/alumni/1.webp',
             // '/gallery/alumni/12.webp',
             // '/gallery/alumni/17.webp',
@@ -82,204 +449,9 @@ const images = [
             '/gallery/alumni/IMG20230414013757.webp',
             '/gallery/alumni/IMG20230414013818.webp',
             '/gallery/alumni/IMG20230414013959.webp',
-            '/gallery/alumni/IMG20230414014013.webp'
         ]
     },
-
-    // {
-    //     "category": "alumni meets",
-    //     "images": [
-    //         "/gallery/alumni meets/IMG_20230414_011041.webp",
-    //         "/gallery/alumni meets/IMG_20230414_011053.webp",
-    //         "/gallery/alumni meets/IMG_20230414_011113.webp",
-    //         "/gallery/alumni meets/IMG_20230414_011116.webp",
-    //         "/gallery/alumni meets/IMG_20230414_011124.webp",
-    //         "/gallery/alumni meets/IMG_20230414_011220.webp",
-    //         "/gallery/alumni meets/IMG_20230414_011245.webp",
-    //         "/gallery/alumni meets/IMG_20230414_011254.webp",
-    //         "/gallery/alumni meets/IMG_20230414_011309.webp",
-    //         "/gallery/alumni meets/IMG_20230414_011320.webp",
-    //         "/gallery/alumni meets/IMG_20230414_011329.webp",
-    //         "/gallery/alumni meets/IMG_20230414_011338.webp",
-    //         "/gallery/alumni meets/IMG_20230414_011346.webp",
-    //         "/gallery/alumni meets/IMG_20230414_011353.webp",
-    //         "/gallery/alumni meets/IMG_20230414_011356.webp",
-    //         "/gallery/alumni meets/IMG_20230414_011411.webp",
-    //         "/gallery/alumni meets/IMG_20230414_011415.webp",
-    //         "/gallery/alumni meets/IMG_20230414_011424.webp",
-    //         "/gallery/alumni meets/IMG_20230414_011432.webp",
-    //         "/gallery/alumni meets/IMG_20230414_011444.webp",
-    //         "/gallery/alumni meets/IMG_20230414_011457.webp",
-    //         "/gallery/alumni meets/IMG_20230414_011501.webp",
-    //         "/gallery/alumni meets/IMG_20230414_011510.webp",
-    //         "/gallery/alumni meets/IMG_20230414_011515.webp",
-    //         "/gallery/alumni meets/IMG_20230414_011519.webp",
-    //         "/gallery/alumni meets/IMG_20230414_011523.webp",
-    //         "/gallery/alumni meets/IMG_20230414_011545.webp",
-    //         "/gallery/alumni meets/IMG_20230414_011620.webp",
-    //         "/gallery/alumni meets/IMG_20230414_011644.webp",
-    //         "/gallery/alumni meets/IMG_20230414_011648.webp",
-    //         "/gallery/alumni meets/IMG_20230414_011652.webp",
-    //         "/gallery/alumni meets/IMG_20230414_011656.webp",
-    //         "/gallery/alumni meets/IMG_20230414_011704.webp",
-    //         "/gallery/alumni meets/IMG_20230414_011713.webp",
-    //         "/gallery/alumni meets/IMG_20230414_011717.webp",
-    //         "/gallery/alumni meets/IMG_20230414_011723.webp",
-    //         "/gallery/alumni meets/IMG_20230414_011731.webp",
-    //         "/gallery/alumni meets/IMG_20230414_011739.webp",
-    //         "/gallery/alumni meets/IMG_20230414_011743.webp",
-    //         "/gallery/alumni meets/IMG_20230414_011754.webp",
-    //         "/gallery/alumni meets/IMG_20230414_011800.webp",
-    //         "/gallery/alumni meets/IMG_20230414_011805.webp",
-    //         "/gallery/alumni meets/IMG_20230414_011814.webp",
-    //         "/gallery/alumni meets/IMG_20230414_011819.webp",
-    //         "/gallery/alumni meets/IMG_20230414_011822.webp",
-    //         "/gallery/alumni meets/IMG_20230414_011827.webp",
-    //         "/gallery/alumni meets/IMG_20230414_011836.webp",
-    //         "/gallery/alumni meets/IMG_20230414_011841.webp",
-    //         "/gallery/alumni meets/IMG_20230414_011849.webp",
-    //         "/gallery/alumni meets/IMG_20230414_011852.webp",
-    //         "/gallery/alumni meets/IMG_20230414_011903.webp",
-    //         "/gallery/alumni meets/IMG_20230414_011906.webp",
-    //         "/gallery/alumni meets/IMG_20230414_011913.webp",
-    //         "/gallery/alumni meets/IMG_20230414_011916.webp",
-    //         "/gallery/alumni meets/IMG_20230414_011920.webp",
-    //         "/gallery/alumni meets/IMG_20230414_011933.webp",
-    //         "/gallery/alumni meets/IMG_20230414_011943.webp",
-    //         "/gallery/alumni meets/IMG_20230414_012002.webp",
-    //         "/gallery/alumni meets/IMG_20230414_012005.webp",
-    //         "/gallery/alumni meets/IMG_20230414_012024.webp",
-    //         "/gallery/alumni meets/IMG_20230414_012035.webp",
-    //         "/gallery/alumni meets/IMG_20230414_012045.webp",
-    //         "/gallery/alumni meets/IMG_20230414_012056.webp",
-    //         "/gallery/alumni meets/IMG_20230414_012102.webp",
-    //         "/gallery/alumni meets/IMG_20230414_012118.webp",
-    //         "/gallery/alumni meets/IMG_20230414_012121.webp",
-    //         "/gallery/alumni meets/IMG_20230414_012129.webp",
-    //         "/gallery/alumni meets/IMG_20230414_012135.webp",
-    //         "/gallery/alumni meets/IMG_20230414_012145.webp",
-    //         "/gallery/alumni meets/IMG_20230414_012200.webp",
-    //         "/gallery/alumni meets/IMG_20230414_012307.webp",
-    //         "/gallery/alumni meets/IMG_20230414_012330.webp",
-    //         "/gallery/alumni meets/IMG_20230414_012342.webp",
-    //         "/gallery/alumni meets/IMG_20230414_012346.webp",
-    //         "/gallery/alumni meets/IMG_20230414_012354.webp",
-    //         "/gallery/alumni meets/IMG_20230414_012730.webp",
-    //         "/gallery/alumni meets/IMG_20230414_012837.webp",
-    //         "/gallery/alumni meets/IMG_20230414_012848.webp",
-    //         "/gallery/alumni meets/IMG_20230414_012859.webp",
-    //         "/gallery/alumni meets/IMG_20230414_012907.webp",
-    //         "/gallery/alumni meets/IMG_20230414_012919.webp",
-    //         "/gallery/alumni meets/IMG_20230414_012927.webp",
-    //         "/gallery/alumni meets/IMG_20230414_013026.webp",
-    //         "/gallery/alumni meets/IMG_20230414_013036.webp",
-    //         "/gallery/alumni meets/IMG_20230414_013048.webp",
-    //         "/gallery/alumni meets/IMG_20230414_013059.webp",
-    //         "/gallery/alumni meets/IMG_20230414_013111.webp",
-    //         "/gallery/alumni meets/IMG_20230414_013126.webp",
-    //         "/gallery/alumni meets/IMG_20230414_013135.webp",
-    //         "/gallery/alumni meets/IMG_20230414_013147.webp",
-    //         "/gallery/alumni meets/IMG_20230414_013151.webp",
-    //         "/gallery/alumni meets/IMG_20230414_013155.webp",
-    //         "/gallery/alumni meets/IMG_20230414_013208.webp",
-    //         "/gallery/alumni meets/IMG_20230414_013211.webp",
-    //         "/gallery/alumni meets/IMG_20230414_013222.webp",
-    //         "/gallery/alumni meets/IMG_20230414_013235.webp",
-    //         "/gallery/alumni meets/IMG_20230414_013246.webp",
-    //         "/gallery/alumni meets/IMG_20230414_013253.webp",
-    //         "/gallery/alumni meets/IMG_20230414_013259.webp",
-    //         "/gallery/alumni meets/IMG_20230414_013359.webp",
-
-    //         "/gallery/alumni meets/IMG_20230414_013404.webp",
-
-    //         "/gallery/alumni meets/IMG_20230414_013413.webp",
-
-    //         "/gallery/alumni meets/IMG_20230414_013417.webp",
-
-    //         "/gallery/alumni meets/IMG_20230414_013426.webp",
-
-    //         "/gallery/alumni meets/IMG_20230414_013429.webp",
-
-    //         "/gallery/alumni meets/IMG_20230414_013438.webp",
-
-    //         "/gallery/alumni meets/IMG_20230414_013446.webp",
-
-    //         "/gallery/alumni meets/IMG_20230414_013457.webp",
-
-    //         "/gallery/alumni meets/IMG_20230414_013503.webp",
-
-    //         "/gallery/alumni meets/IMG_20230414_013508.webp",
-
-    //         "/gallery/alumni meets/IMG_20230414_013514.webp",
-
-    //         "/gallery/alumni meets/IMG_20230414_013519.webp",
-
-    //         "/gallery/alumni meets/IMG_20230414_013523.webp",
-
-    //         "/gallery/alumni meets/IMG_20230414_013530.webp",
-
-    //         "/gallery/alumni meets/IMG_20230414_013535.webp",
-
-    //         "/gallery/alumni meets/IMG_20230414_013543.webp",
-
-    //         "/gallery/alumni meets/IMG_20230414_013552.webp",
-
-    //         "/gallery/alumni meets/IMG_20230414_013556.webp",
-
-    //         "/gallery/alumni meets/IMG_20230414_013608.webp",
-
-    //         "/gallery/alumni meets/IMG_20230414_013616.webp",
-
-    //         "/gallery/alumni meets/IMG_20230414_013622.webp",
-
-    //         "/gallery/alumni meets/IMG_20230414_013634.webp",
-
-    //         "/gallery/alumni meets/IMG_20230414_013645.webp",
-
-    //         "/gallery/alumni meets/IMG_20230414_013650.webp",
-
-    //         "/gallery/alumni meets/IMG_20230414_013656.webp",
-
-    //         "/gallery/alumni meets/IMG_20230414_013702.webp",
-
-    //         "/gallery/alumni meets/IMG_20230414_013705.webp",
-
-    //         "/gallery/alumni meets/IMG_20230414_013711.webp",
-
-    //         "/gallery/alumni meets/IMG_20230414_013713.webp",
-
-    //         "/gallery/alumni meets/IMG_20230414_013722.webp",
-
-    //         "/gallery/alumni meets/IMG_20230414_013727.webp",
-
-    //         "/gallery/alumni meets/IMG_20230414_013752.webp",
-
-    //         "/gallery/alumni meets/IMG_20230414_013757.webp",
-
-    //         "/gallery/alumni meets/IMG_20230414_013807.webp",
-
-    //         "/gallery/alumni meets/IMG_20230414_013813.webp",
-
-    //         "/gallery/alumni meets/IMG_20230414_013818.webp",
-
-    //         "/gallery/alumni meets/IMG_20230414_013835.webp",
-
-    //         "/gallery/alumni meets/IMG_20230414_013903.webp",
-
-    //         "/gallery/alumni meets/IMG_20230414_013939.webp",
-
-    //         "/gallery/alumni meets/IMG_20230414_013950.webp",
-
-    //         "/gallery/alumni meets/IMG_20230414_013959.webp",
-
-    //         "/gallery/alumni meets/IMG_20230414_014009.webp",
-
-    //         "/gallery/alumni meets/IMG_20230414_014013.webp",
-
-    //         "/gallery/alumni meets/IMG-20220607-wa0021.webp",
-    //     ]
-    // },
-
 ]
 
 export default images;
+

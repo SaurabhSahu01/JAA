9610e019c6fdba38c17328c9863285ee463c3ec9
SaurabhSahu01 <saurabhsahu794@gmail.com>
2025-04-10 22:41:51 +0530

    firebase config updated

--- firebase.config.js
+++ firebase.config.js
@@ -1,5 +1,5 @@
 export const firebaseConfig = {
-    apiKey: "AIzaSyD5nCc4a1OK8XE88GqwT3mmPBhzfBe_Z40",
+    apiKey: process.env.NEXT_PUBLIC_USER_PRIVATE_KEY,
     authDomain: "jnu-alumni-association.firebaseapp.com",
     projectId: "jnu-alumni-association",
     storageBucket: "jnu-alumni-association.appspot.com",
@@ -12,7 +12,7 @@ export const adminFirebaseConfig ={
     "type": "service_account",
     "project_id": "jnu-alumni-association",
     "private_key_id": "3711523afa7d7923df581933b761bcdcf15ca427",
-    "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQDoIddq2l1pTl3n\nFXAArNEOzgY6gLDISxhNR8iY2igdWEB9oGgz46yFXbuo0s6lZz+1K1yXF9aDjiir\n/+NZ2wBO8iY3C+Fo13oIqJWtb3jWqpsR3AuyPLgGfo9Jno+AN2oOWlxCROGQaQGH\nCV1Erc4zpdaiF9DjPGIjVL/WrDryAAdFdOlkAX0rA3vArsPerqsAYSrj4XdjZvnK\nT7AY9YCc3eoTLsNaLfJJYrmOf1mArABdFO2Nol9SqJESLUuEvF5/Jqjo+oxXeADC\nXvmCqf6V0YHFWrXvLXRz9ZqXUDEW0StZEn4psnlbj4n9ASfOkAa68EBZNmLsmS5N\necbhX5rrAgMBAAECggEAZudtpAA2bbLgXMr7kkkKCiqZx0B6XegwVz3ttpjrNIsF\nV8uOtgYFWj/hq2b8VYWuT8Vvr8lQ/pO8AveWVSSI//ZHq5pYLA5tmj4F0FM8ucj6\nolGuW66gE7zyDzw7uCaYnNFA2d3IMqjiFPc5QfNcmDe5rLSrFLkVdj0hJ+zE2rcB\nRaBacO56PnKEzBmpBny+RiJO+dMrryE3PlGuRaq9KPRF5P7WNRElB7QqhoejDv+R\n04yzBTJc2q86RYHbgKwPbqRUDy5Qm7jjlqIaICvH3lL43/mW8/4nQsR6G3v/qg4P\nHSCE5gWtNgELIFBntlaXDDmY7g74msXyqRqIIx1PhQKBgQDsWJK/TT8Ji8W7Mx3s\npN/uCTOGJft8LTF4AyPLksBNLc4z4ee69UnfS3BX9LOVr8VTP5hye6RimmQvrSEL\nVKYbTRHxFhu828HmO2/H2PezXfNItCMYgwAv2aRJXunvC7DA7ojsH//vArEJTs0X\nU2ju84Ro/QrlDtrd+F4xL8Qa9wKBgQD7b5Az0H8PCJK72vexg98Tt5fC1RSag790\nRhzObQ8vRMVBNPVtLHHkZbufrU/yjB3IBTyFWHbWmsqeBWadLB8FNDlpjwa6onTE\nW6C94mixvZzJNVvFCxkeMY71zMlTbIdJHqjDb39gxbMMBAUE0jWAO9W6suY2kbCS\nmHHPEccurQKBgQDDyiBPL9u/ubVk/dSVXIRROfgf8kkiNoIbjGZT109M4FxI6KTC\nYT8AJwt7IbC/mOvbqfxNIpYsRQAn6sFMFKTCSxdI4+q4ikhXxPc11Exmn7bRjMDi\nJmu6OA775Q/gQov7V9+/1EZ9pnooXspRv3xXLw6Kts0Wsjan2X+WOZ64QwKBgGj9\nrvNT7ppydAMpbiCb9jyffymGHHJg7UM6O7Ee7SZJyyxtVsZdtrkumegVULrxM0Zg\nmWL9C7KVNGJD0xNyZwmfQxIG/UJVr/iGYlqn/huqF5wVQdrHxZdMDmwpNTA2s08f\nFE13c3MPWrJ7wzS+KkT/t0GcjnDe6SJtsahLviaJAoGBANcVtzQFVL8mCRTwY9m3\ntfX7BzNWQ++ntegqAZfUg+kOezk3B7zXmTis0UhNxhyIGesFJF6my/YpTaWHDa9Q\nGbzJnjNte43TdHW7LStsXsk6A7vkma6oF0Y1UZQoMKJJzISNywGd62x86GQSEYbf\nCuWo9RdtlOOgoSyYfClwRxjj\n-----END PRIVATE KEY-----\n",
+    "private_key": process.env.NEXT_PUBLIC_ADMIN_PRIVATE_KEY,
     "client_email": "firebase-adminsdk-sxoes@jnu-alumni-association.iam.gserviceaccount.com",
     "client_id": "104407404527396492428",
     "auth_uri": "https://accounts.google.com/o/oauth2/auth",

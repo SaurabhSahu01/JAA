66eeb377f4afa648f997193525f7da1480d45dd9
SaurabhSahu01 <saurabhsahu794@gmail.com>
2025-04-10 23:34:41 +0530

    firebase config

--- firebase.config.js
+++ firebase.config.js
@@ -1,29 +1,29 @@
 export const firebaseConfig = {
-    apiKey: process.env.NEXT_PUBLIC_USER_PRIVATE_KEY,
-    authDomain: "jnu-alumni-association.firebaseapp.com",
-    projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
-    storageBucket: "jnu-alumni-association.appspot.com",
-    messagingSenderId: "382439048728",
-    appId: "1:382439048728:web:a2504dad6fb65d2d5de56d",
-    measurementId: "G-WCCWJ05M36"
-};
+  apiKey: "AIzaSyD5nCc4a1OK8XE88GqwT3mmPBhzfBe_Z40",
+  authDomain: "jnu-alumni-association.firebaseapp.com",
+  projectId: "jnu-alumni-association",
+  storageBucket: "jnu-alumni-association.appspot.com",
+  messagingSenderId: "382439048728",
+  appId: "1:382439048728:web:a2504dad6fb65d2d5de56d",
+  measurementId: "G-WCCWJ05M36"
+};;
 
 export const adminFirebaseConfig ={
-    "type": "service_account",
-    "project_id": process.env.NEXT_PUBLIC_PROJECT_ID,
-    "private_key_id": "3711523afa7d7923df581933b761bcdcf15ca427",
-    "private_key": process.env.NEXT_PUBLIC_ADMIN_PRIVATE_KEY,
-    "client_email": "firebase-adminsdk-sxoes@jnu-alumni-association.iam.gserviceaccount.com",
-    "client_id": "104407404527396492428",
-    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-    "token_uri": "https://oauth2.googleapis.com/token",
-    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-    "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-sxoes%40jnu-alumni-association.iam.gserviceaccount.com",
-    "universe_domain": "googleapis.com"
-  };
+  "type": "service_account",
+  "project_id": "jnu-alumni-association",
+  "private_key_id": "863dc7ccd8f98d4f39a2bb842dc66e471d7e6e17",
+  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQCrYtWRnKmrePiR\nHipvQmH5U6/o1zczCEXbIlUc2IaSv6xBNMrkkn4jJboygpQWmxIVL6LBiU+mXr+4\nkcNx/gTyePvDnZqXrIOlaol462WGptBVy5evMpUzxOAtA4YYSGj5k+KDyPruWdOC\nykURMGCy0gm8erWarr+iW/Y+f6HRZ9Rbq2T+3aJ0Z/o/woIUzjQmgMuziljjtmHL\n7JwAskV0zgx6/0wsE13kVJaX67KdN5nX32MnYmJ+6ZiHVlycC6EjIhBpF8CY7vn7\nWBZLzDYB0ATWBMyfYHsifwGRQVbRQAdBtRy0pbR6d26km+clcldb5rxbZbN9QJ6I\nsbX0qnj5AgMBAAECggEAMKG8cXOvb5FFk0QojWacT3fwSBVdu/TigaJ13VgRo62Q\n7vXQhtiMw2NuuOt1ow6ArJNWTy2r9M0ZAQIXoA6RD87FvgXIOrbAql0XQkElj3Zh\nUp8RXSzwyn5YkF1aHSTa6MsOcQ/EKINDse68d3qvTb6s9ZcqVgi1JxiKQMTNwZIK\nRb1XfqGiM97of5cXkuEl52LHfF9Xw1xWHXA6ftVEtfG8BXrWmhdw0axrCBjHGbzj\n66nt9CkVfhJGw8rxoJg+Xi5gm/H2FOU2pvKWwr7w/bQ2qeKMzJCpMOChehoa3bpU\n9K0Ti84V+X/e6Z9VBDc/Hc03apiMC3X4fQXkkvZNYwKBgQDezvR7UfjbIWsEphi9\nYaSdqyYoTAzmddKX5K6GVHes22GsGu0PnFSGvdsRrXfAi2L5pGoUsipuIgGOdKS2\nkPOTec+JyOS1Bdhyc/Fi4VH91c7kN7AqH+QdetgDKJxOtEJmrIVhNle94hDXDlg9\nHhczu4h9vIgtpsiaCWGLtVZ/0wKBgQDE6tTPNCLi54PjkRex2sOnQVrxYwExY4sS\n5pkA2c35gJqcMxJvX3XlPP4eH+p5q/ZJ1s0XQT6RJ+dKlineU0Hcfz//67SB6o2l\nbvMK7AbZlV+pG3mC7Iw4hT8fy4nn+o8lPS/Izof58jgnVPuAL6xQGND7QlLczffU\nOE1+OqKwgwKBgQCLP+6+MAIG+gDHfANUQPU/8Ro87yLU2ehPNAX7QEk3SDjn9kAY\nlIyjVX7XsoF4XeTyvtXpr1wjTq+IV9AlpbwFfIi/xUVi+Y9IILxJHyc+LGVMbp10\nBlBsOD1O5A+khLZHMyV9FxiJPQNnVLjDIc5Ab8EjTDQTy/vtgVVsml3ALwKBgCJi\nr06PWBgvPUSbrXuNK+Uk8HB+TVMGgt6GDemsHt+WqWjlMi7oiHnDMMyp3hPpB1TV\ngXKyHe7S/Px1RFJiT3nlbpON4TNedeWRfjdGB9wuzUmGVq2aCNJF0kQHEsnDYwOp\nr5JtHilzNoMHv/MeYASMkNGPP1D3VnNkcYO9UXNpAoGAP/gzTliN4mM2KCl243iE\n4GPmPmgA8MJHlmiuOtrscgm7fRZd+HKcCyJ4gVrYfg/8yc0wRygEfxruPVON5OpQ\n+FTZMxenrQVDGoE/nUR5pxOxJ3UhKD5Dxirvf2QAzx/UnUg4a+mMMA6LYrytEWf5\neh9paSQmoIeiqI6q/sZ5PJM=\n-----END PRIVATE KEY-----\n",
+  "client_email": "firebase-adminsdk-sxoes@jnu-alumni-association.iam.gserviceaccount.com",
+  "client_id": "104407404527396492428",
+  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+  "token_uri": "https://oauth2.googleapis.com/token",
+  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-sxoes%40jnu-alumni-association.iam.gserviceaccount.com",
+  "universe_domain": "googleapis.com"
+};
 
 export const firebaseStorageBucket = "gs://jnu-alumni-association.appspot.com";
 
-export const webAPIKey = process.env.NEXT_PUBLIC_WEB_API_KEY;
+export const webAPIKey = "AIzaSyD5nCc4a1OK8XE88GqwT3mmPBhzfBe_Z40";
 
-export const JWT_SECRET = process.env.NEXT_PUBLIC_JWT_SECRET;
+export const JWT_SECRET = "sadkfhalsdfjoacid";
